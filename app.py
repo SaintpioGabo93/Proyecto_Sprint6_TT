9dd8543de77a246a10ceac04db26e544c1cd97f3
Saintpiogabo93 <tocata_fuga_dorian@hotmail.com>
2024-10-06 11:29:23 -0600

    Se crean botones de verificacion

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import streamlit as st
 df_autos = pd.read_csv("vehicles_us.csv")
 boton_histograma = st.button("Construir Histograma")
 boton_dispersion = st.button("Construir Grafico de Dispersion")
+casilla_histograma = st.checkbox("Construir casilla de verificacion histograma")
+casilla_dispersion = st.checkbox("Construir casilla de verificacion grafico de dispersion")
 
 # Creamos el boton de histograma
 if boton_histograma:
@@ -17,5 +19,17 @@ if boton_histograma:
 #Creamo el boton de grafico de dispesion
 if boton_dispersion:
     st.write("Creamos un grafico de dispersion para anuncios de coches")
-    fig = px.histogram(df_autos, x="odometer", y="price")
+    fig = px.scatter(df_autos, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Creamos el boton de verificacion histograma 
+if casilla_histograma:
+    st.write("Construccion de un histograma para la columna odometro con boton de verificacion")
+    fig = px.histogram(df_autos, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Creamos el boton de verificacion grafico de dispersion
+if casilla_histograma:
+    st.write("Construccion de un histograma para la columna odometro con boton de verificacion")
+    fig = px.scatter(df_autos, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
