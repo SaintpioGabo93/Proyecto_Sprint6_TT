750b45ecc05433d9be8e93b704cc506a05bcbd3d
Saintpiogabo93 <tocata_fuga_dorian@hotmail.com>
2024-10-06 11:14:31 -0600

    Se reviso que cumpliera con PREP8

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+#Importamos las librerias
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-df_autos = pd.read_csv('vehicles_us.csv')
-boton_histograma = st.button('Construir Histograma')
+# Creamos las variables para los objetos
+df_autos = pd.read_csv("vehicles_us.csv")
+boton_histograma = st.button("Construir Histograma")
+boton_dispersion = st.button("Construir Grafico de Dispersion")
 
+# Creamos el boton de histograma
 if boton_histograma:
-    st.write('Creamos un histograr para anuncios de coches')
-    fig = px.histogram(df_autos, x='odometer')
+    st.write("Creamos un histograma para anuncios de coches")
+    fig = px.histogram(df_autos, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+#Creamo el boton de grafico de dispesion
+if boton_dispersion:
+    st.write("Creamos un grafico de dispersion para anuncios de coches")
+    fig = px.histogram(df_autos, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
